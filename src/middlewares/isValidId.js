5d015150c2f1ca6ccb8a5e180b5ad8b1ccf29e27
Andrii KRUPKO <jobsdetekt.ak@gmail.com>
2025-07-02 14:31:19 +0300

    valid

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -4,9 +4,9 @@ export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(contactId)) {
-    return res.status(400).json({
-      status: 400,
-      message: `Invalid ID format: ${contactId}`,
+    return res.status(404).json({
+      status: 404,
+      message: "Contact not found",
     });
   }
 

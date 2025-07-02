cb2fefcffa16e9fb2c758af328d34661f9b0fc52
Andrii KRUPKO <jobsdetekt.ak@gmail.com>
2025-07-02 14:08:32 +0300

    num 4

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,14 @@
 export function errorHandler(err, req, res, next) {
   console.error("Error caught:", err);
+
+  if (err.name === "CastError" && err.path === "_id") {
+    return res.status(404).json({
+      status: 404,
+      message: "Contact not found",
+      data: { message: "Contact not found" },
+    });
+  }
+
   const status = err.status || 500;
   const message = err.message || "Something went wrong";
 

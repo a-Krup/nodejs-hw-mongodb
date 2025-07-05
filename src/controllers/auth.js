3b9f148b67ca3f6b805afafb4917cae3fd26b4af
Andrii KRUPKO <jobsdetekt.ak@gmail.com>
2025-07-05 10:49:10 +0300

    false

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -196,7 +196,7 @@ export const sendResetEmail = async (req, res, next) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: parseInt(process.env.SMTP_PORT, 10),
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,

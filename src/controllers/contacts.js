3eaac1eb508fd1643a6813338d773457d4435f81
Andrii KRUPKO <jobsdetekt.ak@gmail.com>
2025-07-12 12:09:54 +0300

    controllers

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -24,10 +24,19 @@ export const getAll = async (req, res, next) => {
       sortOrder
     );
 
+    
     res.status(200).json({
       status: 200,
       message: "Successfully found contacts!",
-      data: result,
+      data: {
+        data: result.data,
+        page: result.page,
+        perPage: result.perPage,
+        totalItems: result.totalItems,
+        totalPages: result.totalPages,
+        hasPreviousPage: result.hasPreviousPage,
+        hasNextPage: result.hasNextPage,
+      },
     });
   } catch (err) {
     next(err);
